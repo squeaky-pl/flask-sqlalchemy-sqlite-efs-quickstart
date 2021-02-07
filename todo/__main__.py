03a95dcca44d4e3485c6e8466c4fb0de10c185d9
Squeaky <squeaky_pl@gmx.com>
2021-02-07 11:23:22 +0100

    introduce sqlalchemy and sqlite

--- todo/__main__.py
+++ todo/__main__.py
@@ -1,6 +1,20 @@
+import pprint
+import uuid
+
+from todo.db import Session
+from todo.db import Todo
+
+
 def todo():
-    return {"todo": "todo"}
+    session = Session()
+    todo = Todo(name=f"My todo {uuid.uuid4()}")
+    session.add(todo)
+    session.commit()
+
+    todos = session.query(Todo)
+
+    return {"todos": [todo.to_json() for todo in todos]}
 
 
 if __name__ == "__main__":
-    print(todo())
+    pprint.pprint(todo())
---
+++ todo/db.py
@@ -0,0 +1,27 @@
+from sqlalchemy import Column
+from sqlalchemy import create_engine
+from sqlalchemy import Integer
+from sqlalchemy import String
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+engine = create_engine("sqlite:////mnt/data/todo.db")
+
+Base = declarative_base()
+
+
+class Todo(Base):
+    __tablename__ = "todo"
+    id = Column(Integer, primary_key=True)
+    name = Column(String)
+
+    def to_json(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+        }
+
+
+Base.metadata.create_all(engine)
+
+Session = sessionmaker(bind=engine)
